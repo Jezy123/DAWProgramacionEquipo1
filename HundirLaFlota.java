0231e806946ef6402baa699db259d0b03c5baca9
carmengm14 <alugar4059@ieselcamincas.org>
2021-12-10 11:47:43 +0100

    hola

--- HundirLaFlota.java
+++ HundirLaFlota.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
 
 public class HundirLaFlota {
 
-    //CREAMOS LOS 1 DE FORMA ALEATORIA PARA INDICAR LAS POSICIONES DE LOS BARCOS.
-    public static void crearBarcos(int numBarcos, int[][] plazasBarco) {
+    public static void crearBarcos(int numBarcos, int[][] numPlazas) {
         int r1, r2;
         int cont = 0;
-        //Transformamos posiciones aleatoria en el tablero, en 1 para indicar que hay un barco.
+    
         while (cont < numBarcos) {
             r1 = (int) (Math.random()*8); 
             r2 = (int) (Math.random()*8);
-            if (plazasBarco[r1][r2] != 1) {
-                plazasBarco[r1][r2]=1;
+            if (numPlazas[r1][r2] != 1) {
+                numPlazas[r1][r2]=1;
                 cont++;
             }
         }
@@ -28,7 +26,7 @@ public class HundirLaFlota {
                 switch (tablero[i][j]) {
                     case 0:
                     case 1:
-                        System.out.print(" ");
+                        System.out.print(".");
                         break;
                     case 2:
                         System.out.print("O ");
@@ -44,28 +42,25 @@ public class HundirLaFlota {
             System.out.println();
         }
     }
-
-
-//EL MAIN
     public static void main(String[] args) {
-        //cremamos el tablero vacio de 8x8
-        int[][] tablero;
-        tablero = new int [8][8];
-        //Rellenamos el tablero con los barcos aleatorios
-        crearBarcos(10, tablero);
-        int numsBarcos = 0;
-        //pedimos al usuario la fila y la columna que quiere ver
-        Scanner sc = new Scanner(System.in);
-        char fila = Utilidades.leerCaracter("Escribe la fila (Letra en Mayus)= ");
-        int columna = Utilidades.leerEntero("Escribe la columna (Numero)= ");
-        sc.close(); 
-        //creamos el FORMATO DE LA TABLA (A - H) (1 - 8)
-        int numFila = fila - 'A';
-        columna--;
 
-        if (tablero[numFila][columna] == 0 || tablero[numFila][columna] == 1){
-            tablero[numFila][columna] += 2;
-        }
+        int[][] tablero;
+        tablero = new int[8][8];
+
+        crearBarcos(10, tablero);
+        
+        imprimirTablero(tablero);
+            char fila = Utilidades.leerCaracter("Escoge una fila (Letra Mayuscula): ");
+            int numColumna = Utilidades.leerEntero("Escoge una columna(Numero): ");
+            int numFila = fila - 'A';
+            numColumna--;
+
+            if (tablero[numFila][numColumna] == 0 || tablero[numFila][numColumna] == 1){
+                tablero[numFila][numColumna] += 2;
+            }
+        
             imprimirTablero(tablero);
-    }
+    }    
 }
+
+
